29450e95c40fc58d2169b5e0572684c482db13d9
HamidAhmed3 <hamid.ahmed3217@gmail.com>
2023-10-08 18:07:07 +0500

    Home, Login, Signup

---
+++ react-ecommerce/src/App.js
@@ -0,0 +1,17 @@
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import Home from './pages/Home';
+import SignupPage from './pages/SignupPage';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Home></Home> */}
+      <SignupPage></SignupPage>
+    </div>
+  );
+}
+
+export default App;
